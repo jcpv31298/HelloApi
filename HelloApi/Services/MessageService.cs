717228d11ea74879fa05224f834fb176b0e8317a
Carlos Paez <carlos31298@gmail.com>
2026-07-29 11:24:24 -0700

    Test: CodeQL add intentional vulnerabiloty code

--- HelloApi/Services/MessageService.cs
+++ HelloApi/Services/MessageService.cs
@@ -2,6 +2,15 @@
 {
     public class MessageService : IMessageService
     {
+        private const string ApiKey = "sk-1234567890abcdef";  // ❌ Secret in code
+        private const string Password = "SuperSecret123!";
+
         public string GetMessage() => "Hello World";
+
+        public string GetApiPassword()
+        {
+            var apiUrl = $"https://api.example.com?key={ApiKey}";
+            return "Hola desde .NET 8 API";
+        }
     }
 }
